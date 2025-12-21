46eedf97cc1bf43c8e4a9e7474e3c22a65d5809b
Dominic Couture <45298797+mastermind-mayhem@users.noreply.github.com>
2025-12-21 16:15:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -50,106 +50,110 @@ else:
     exit()
 
 for ssymbol in symbol:
-    print(ssymbol)
-    
-    # Calculate data
-    # Create Models
-    # Fetch data
-    start_date = datetime.now() - timedelta(days=days)
-    end_date = datetime.now()
-    ticker = yf.Ticker(ssymbol)
-    datas = ticker.history(start=start_date, end=end_date)
+    try:    
+        print(ssymbol)
+        
+        # Calculate data
+        # Create Models
+        # Fetch data
+        start_date = datetime.now() - timedelta(days=days)
+        end_date = datetime.now()
+        ticker = yf.Ticker(ssymbol)
+        datas = ticker.history(start=start_date, end=end_date)
 
-    # Prepare data
-    cps = datas['Close'].tolist()
-    count = len(cps)
-    x = np.linspace(1, count, count).reshape(-1, 1)
-    close = np.array(cps)
+        # Prepare data
+        cps = datas['Close'].tolist()
+        count = len(cps)
+        x = np.linspace(1, count, count).reshape(-1, 1)
+        close = np.array(cps)
 
-    # Polynomial Regression
-    poly = PolynomialFeatures(degree=degree)
-    X_poly = poly.fit_transform(x)
-    poly.fit(X_poly, close)
-    pol = LinearRegression()
-    pol.fit(X_poly, close)
+        # Polynomial Regression
+        poly = PolynomialFeatures(degree=degree)
+        X_poly = poly.fit_transform(x)
+        poly.fit(X_poly, close)
+        pol = LinearRegression()
+        pol.fit(X_poly, close)
 
-    # Linear Regression
-    lin = LinearRegression()
-    lin.fit(x, close)
+        # Linear Regression
+        lin = LinearRegression()
+        lin.fit(x, close)
 
-    # Calculate error between linear and polynomial regression
-    err = []
-    for i in range(count):
-        i = np.array([[i]])
-        linear = lin.predict(i)[0]
-        err.append(linear - close[i][0][0])
-    mean = stats.mean(err)
-    stdev = stats.stdev(err)
+        # Calculate error between linear and polynomial regression
+        err = []
+        for i in range(count):
+            i = np.array([[i]])
+            linear = lin.predict(i)[0]
+            err.append(linear - close[i][0][0])
+        mean = stats.mean(err)
+        stdev = stats.stdev(err)
 
-    # Predict
-    guess = np.array([[count + 1]])
-    polyguess = pol.predict(poly.fit_transform(guess))[0]
-    linguess = lin.predict(guess)[0]
-    prederr = polyguess - linguess
-    zscore = abs((prederr - mean) / stdev)
-    azscore = abs(((cps[-1] - linguess) - mean) / stdev)
-    print(azscore)
+        # Predict
+        guess = np.array([[count + 1]])
+        polyguess = pol.predict(poly.fit_transform(guess))[0]
+        linguess = lin.predict(guess)[0]
+        prederr = polyguess - linguess
+        zscore = abs((prederr - mean) / stdev)
+        azscore = abs(((cps[-1] - linguess) - mean) / stdev)
+        print(azscore)
 
-    # Condition 1: linguess > actual latest value
-    print(cps[-1], linguess)
-    if linguess < cps[-1]:
-        content = f"""{ssymbol}  Fail-Condition 1: Linear Regression Below Close Price
-    Close Price: ${cps[-1]:.2f}
-    Model Prediction: ${linguess:.2f}
-    """
-        webhook = DiscordWebhook(url=WEBHOOK_URL, content=content)
-        response = webhook.execute()
-        if response and response.status_code in [200, 204]:
-            print("Image Sent to Discord")
+        # Condition 1: linguess > actual latest value
+        print(cps[-1], linguess)
+        if linguess < cps[-1]:
+            content = f"""{ssymbol}  Fail-Condition 1: Linear Regression Below Close Price
+        Close Price: ${cps[-1]:.2f}
+        Model Prediction: ${linguess:.2f}
+        """
+            webhook = DiscordWebhook(url=WEBHOOK_URL, content=content)
+            response = webhook.execute()
+            if response and response.status_code in [200, 204]:
+                print("Image Sent to Discord")
+            else:
+                print("Send Fail")
+            continue
+        # Condition 2: Z-score
+        elif azscore <= zlimit:
+            content = f"""{ssymbol}  Fail-Condition 2: Close Price Zscore Below Threshold
+        Z-Score: {azscore:.2f}
+        Z-Score Limit: {zlimit:.2f}
+        """
+            webhook = DiscordWebhook(url=WEBHOOK_URL, content=content)
+            response = webhook.execute()
+            if response and response.status_code in [200, 204]:
+                print("Image Sent to Discord")
+            else:
+                print("Send Fail")
+            continue
+
+        plt.plot(x, close, color='red', label='Actual')
+        # plt.plot(x, pol.predict(poly.fit_transform(x)),
+        #         color='blue', label='PolyReg')
+        plt.plot(x, lin.predict(x), color='green', label='LinReg')
+        plt.xlabel('Days')
+        plt.ylabel('Price')
+        plt.legend()
+        filename = "stockplot.png"
+        plt.savefig(filename, dpi=300, bbox_inches='tight', facecolor='#ababab', transparent=True)
+        plt.close() 
+
+        content = f"""
+        Close Price: ${cps[-1]:.2f}
+        Close Price Z-Score: {azscore:.2f}
+        Predicted Price: ${polyguess:.2f}
+        """
+        webhook = DiscordWebhook(url=WEBHOOK_URL, content=f"It's a great time to buy {ssymbol}") #
+        with open(filename, "rb") as f:
+            webhook.add_file(file=f.read(), filename=filename) #
+        embed = DiscordEmbed(title=ssymbol, description=content, color="03b2f8")
+        embed.set_image(url=f"attachment://{filename}")  #
+        webhook.add_embed(embed)  
+        response = webhook.execute() 
+        os.remove(filename)
+
+        if response and response.status_code in [200, 204]: 
+            print(f"Image sent to Discord - {time.ctime()}")
         else:
-            print("Send Fail")
+            print(f"Failed to send image to Discord. Status code: {response.status_code if response else 'N/A'}")
+            print(f"Response: {response.text if response else 'N/A'}")
+    except Exception as e:
+        print(f"An error occurred for {ssymbol}: {e}")
         continue
-    # Condition 2: Z-score
-    elif azscore <= zlimit:
-        content = f"""{ssymbol}  Fail-Condition 2: Close Price Zscore Below Threshold
-    Z-Score: {zscore:.2f}
-    Z-Score Limit: {zlimit:.2f}
-    """
-        webhook = DiscordWebhook(url=WEBHOOK_URL, content=content)
-        response = webhook.execute()
-        if response and response.status_code in [200, 204]:
-            print("Image Sent to Discord")
-        else:
-            print("Send Fail")
-        continue
-
-    plt.plot(x, close, color='red', label='Actual')
-    plt.plot(x, pol.predict(poly.fit_transform(x)),
-            color='blue', label='PolyReg')
-    plt.plot(x, lin.predict(x), color='green', label='LinReg')
-    plt.xlabel('Days')
-    plt.ylabel('Price')
-    plt.legend()
-    filename = "stockplot.png"
-    plt.savefig(filename, dpi=300, bbox_inches='tight', facecolor='#ababab', transparent=True)
-    plt.close() 
-
-    content = f"""
-    Close Price: ${cps[-1]:.2f}
-    Close Price Z-Score: {azscore:.2f}
-    Predicted Price: ${polyguess:.2f}
-    """
-    webhook = DiscordWebhook(url=WEBHOOK_URL, content=f"It's a great time to buy {ssymbol}") #
-    with open(filename, "rb") as f:
-        webhook.add_file(file=f.read(), filename=filename) #
-    embed = DiscordEmbed(title=ssymbol, description=content, color="03b2f8")
-    embed.set_image(url=f"attachment://{filename}")  #
-    webhook.add_embed(embed)  
-    response = webhook.execute() 
-    os.remove(filename)
-
-    if response and response.status_code in [200, 204]: 
-        print(f"Image sent to Discord - {time.ctime()}")
-    else:
-        print(f"Failed to send image to Discord. Status code: {response.status_code if response else 'N/A'}")
-        print(f"Response: {response.text if response else 'N/A'}")
